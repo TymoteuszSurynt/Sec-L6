d12cca78659edf9ac1e95815f0bbeb63363f17b4
Tymoteusz Surynt <tymoteusz.surynt@gmail.con>
2017-12-10 10:04:43 +0100

    Changes

--- bserver.cpp
+++ bserver.cpp
@@ -3,108 +3,147 @@
 #include <openssl/pem.h>
 #include <crypt.h>
 #include <fstream>
-#include <iostream>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <stdlib.h>
 #include <unistd.h>
-#include <stdio.h>
+#include <ctime>
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
-    int option, size, passwordSize = 12, saltSize = 4;
-    string password, salt, password2;
-    BIGNUM *bn;
-    RSA *key;
-    BIO *publicKey, *privateKey;
-    unsigned long e = RSA_F4;
+void getPassword(int passwordSize, int saltSize) {
 
+    int option;
+    ofstream config;
+    string password, salt;
+    cout << "1-Own password 2-Generated Password(default)" << endl;
+    cin >> option;
+    if (option == 1) {
+        cout << "Enter password:" << endl;
+        cin >> password;
+    } else {
+        srand(time(NULL));
+        for (int i = 0; i < passwordSize; i++) {
+            password += (char) (rand() % 105 + 21);
+        }
+        cout << "Your password:" << endl;
+        cout << password << endl;
+        cout << "Do not forget your password" << endl;
+
+    }
+    for (int i = 0; i < saltSize; ++i) {
+        salt += (char) (rand() % 105 + 21);
+    }
+    config.open("config");
+    config << crypt(password.c_str(), ("$6$" + salt).c_str()) << endl << salt;
+    config.close();
+}
+
+int keySize() {
+    int option;
+    cout << "Choose size: 1-2048(default) 2-4096 3-8192 4-16384" << endl;
+    cin >> option;
+    if (option == 2) {
+        return 4096;
+    } else if (option == 3) {
+        return 8192;
+    } else if (option == 4) {
+        return 16384;
+    } else {
+        return 2048;
+    }
+}
+
+void generateKey(int size) {
+    BIGNUM *bn = BN_new();
+    const BIGNUM *N = BN_new();
+    const BIGNUM *d = BN_new();
+    const BIGNUM *e = BN_new();
+    RSA *key = RSA_new();
+    ofstream file1, file2;
+    if (BN_set_word(bn, RSA_F4) != 1) {
+        cout << "Something went wrong!" << endl;
+        exit(1);
+    }
+    clock_t start;
+    start = clock();
+    if (RSA_generate_key_ex(key, size, bn, nullptr) != 1) {
+        cout << "Something went wrong!" << endl;
+        exit(1);
+    }
+    cout << "Generating key: " << (clock() - start) / (double) CLOCKS_PER_SEC << endl;
+    RSA_get0_key(key, &N, &e, &d);
+
+    file1.open("publicKey");
+    if (file1.is_open()) {
+        file1 << BN_bn2hex(N) << endl << BN_bn2hex(e);
+        file1.close();
+    } else {
+        cout << "File problem" << endl;
+        exit(1);
+    }
+    file2.open("privateKey");
+    if (file2.is_open()) {
+        file2 << BN_bn2hex(N) << endl << BN_bn2hex(d);
+        file2.close();
+    } else {
+        cout << "File problem" << endl;
+        exit(1);
+    }
+
+    RSA_free(key);
+    BN_free(bn);
+}
+
+int main(int argc, char *argv[]) {
+    int option, size, bufferSize = 17000;
+    string nienawidzePisacWc = "1";
     cout << "Type: 1-Setup 2-Sign(default)" << endl;
     cin >> option;
     if (option == 1) {
-        ofstream config;
-        cout << "Choose size: 1-2048(default) 2-4096 3-8192 4-16384" << endl;
-        cin >> option;
-        if (option == 2) {
-            size = 4096;
-        } else if (option == 3) {
-            size = 8192;
-        } else if (option == 4) {
-            size = 16384;
-        } else {
-            size = 2048;
-        }
-        cout << "1-Own password 2-Generated Password(default)" << endl;
-        cin >> option;
-        config.open("config");
-        if (option == 1) {
-            cout << "Enter password:" << endl;
-            cin >> password;
-        } else {
-            srand(time(NULL));
-            for (int i = 0; i < passwordSize; i++) {
-                password += (char) (rand() % 105 + 21);
-            }
-            cout << "Your password:" << endl;
-            cout << password << endl;
-            cout << "Do not forget your password" << endl;
-
-        }
-        for (int i = 0; i < saltSize; ++i) {
-            salt += (char) (rand() % 105 + 21);
-        }
-        password = crypt(password.c_str(), ("$6$" + salt).c_str());
-        config << password << endl << salt;
-        config.close();
-        bn = BN_new();
-        if (BN_set_word(bn, e) != 1) {
-            cout << "Something went wrong!" << endl;
-            exit(1);
-        }
-        key = RSA_new();
-        if (RSA_generate_key_ex(key, size, bn, nullptr) != 1) {
-            cout << "Something went wrong!" << endl;
-            exit(1);
-        }
-        publicKey = BIO_new_file("publicKey.pem", "w+");
-        privateKey = BIO_new_file("privateKey.pem", "w+");
-        if (PEM_write_bio_RSAPublicKey(publicKey, key) != 1) {
-            cout << "Something went wrong!" << endl;
-            exit(1);
-        }
-        if (PEM_write_bio_RSAPrivateKey(privateKey, key, nullptr, nullptr, 0, nullptr, nullptr) != 1) {
-            cout << "Something went wrong!" << endl;
-            exit(1);
-        }
-        BIO_free_all(privateKey);
-        BIO_free_all(publicKey);
-        RSA_free(key);
-        BN_free(bn);
+        size = keySize();
+        generateKey(size);
+        getPassword(12, 4);
 
     } else {
+        string password, salt, password2;
         ifstream config("config");
+
         if (config.is_open()) {
             getline(config, password);
             getline(config, salt);
             cout << "Enter password" << endl;
             cin >> password2;
             if (crypt(password2.c_str(), ("$6$" + salt).c_str()) == password) {
-                int server, client; // socket file descriptors
+                string privateK, Nstring;
+                ifstream privateKey;
+                BIGNUM *N = BN_new();
+                BIGNUM *d = BN_new();
+                BIGNUM *x = BN_new();
+                BIGNUM *r = BN_new();
+                privateKey.open("privateKey");
+                if (privateKey.is_open()) {
+                    getline(privateKey, Nstring);
+                    getline(privateKey, privateK);
+                    privateKey.close();
+                }
+                BN_hex2bn(&N, Nstring.c_str());
+                BN_hex2bn(&d, privateK.c_str());
+
+                char buffer[bufferSize];
+                int server;
+                int client[10];
+                int clientNum = 0;
                 int portNum;
-                string echo;
-                char buffer[1024]; // buffer to transmit
-                bool isExit = false; // var fo continue infinitly
+
                 struct sockaddr_in server_addr;
                 socklen_t size;
 
 
                 if(argc<2){
-                    cout<<"No port added, running on default 100"<<endl;
-                    portNum=100;
+                    cout << "No port added, running on default 4000" << endl;
+                    portNum = 4000;
                 }else{
                     portNum=atoi(argv[1]);
                 }
@@ -127,44 +166,34 @@ int main(int argc, char *argv[]) {
                 }
                 size = sizeof(server_addr);
                 listen(server, 1);
-                client = accept(server, (struct sockaddr *) &server_addr, &size);
-                if (client < 0) {
-                    cout << "Error: accept" << endl;
+                while (clientNum < 10) {
+                    client[clientNum] = accept(server, (struct sockaddr *) &server_addr, &size);
+                    if (client < 0) {
+                        cout << "Error: accept" << endl;
+                    }
+                    if (client > 0) {
+                        strcpy(buffer, "Connected");
+                        send(client[clientNum], buffer, bufferSize, 0);
+                        recv(client[clientNum], buffer, bufferSize, 0);
+                        clock_t start;
+                        start = clock();
+                        BN_hex2bn(&x, buffer);
+                        BN_gcd(r, N, x, BN_CTX_new());
+                        if (!nienawidzePisacWc.compare(BN_bn2dec(r))) {
+                            BN_mod_exp(r, x, d, N, BN_CTX_new());
+                            strcpy(buffer, BN_bn2hex(r));
+                        } else {
+                            strcpy(buffer, "Wrong message");
+                        }
+                        cout << "Signing: " << (clock() - start) / (double) CLOCKS_PER_SEC << endl;
+                        send(client[clientNum], buffer, bufferSize, 0);
+                        close(client[clientNum]);
+                    }
                 }
-
-                while (client > 0) {
-                    strcpy(buffer, "Connected");
-                    send(client, buffer, 1024, 0);
-                    do {
-                        cout << "\nClient: ";
-                        echo = "";
-                        do {
-                            // wait the request from client
-                            recv(client, buffer, 1024, 0);
-                            cout << buffer << " ";
-                            if (*buffer == '#') {
-                                // exit loop and say goodbye (It's a polite server :D)
-                                isExit = true;
-                                *buffer = '*';
-                                echo = "Goodbye!";
-                            } else if ((*buffer != '#') && (*buffer != '*')) {
-                                // concatenate the echo string to response to the client
-                                echo += buffer;
-                                echo += " ";
-                            }
-                        } while (*buffer != '*');
-                        // copy the echo string to the buffer
-                        sprintf(buffer, "%s", echo.c_str());
-                        // send the message to the client
-                        send(client, buffer, 1024, 0);
-                    } while (!isExit);
-                    cout << "\n\n=> Connection terminated with IP "
-                         << inet_ntoa(server_addr.sin_addr);
-                    close(client);
-                    cout << "\nGoodbye..." << endl;
-                    exit(1);
-
-                }
+                BN_free(N);
+                BN_free(d);
+                BN_free(x);
+                BN_free(r);
                 close(server);
 
             } else {
